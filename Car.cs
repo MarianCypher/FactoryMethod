677ed7fb7cc7c4aeeeb9d34c2b7fad0ebb2d5d5d
MarianCypher <p.rybczynski@hotmail.com>
2024-01-31 20:17:52 +0100

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Racing;
+
+namespace Automotive;
+/* zawiera klase Car z modyfikatorami dostepu sealed oraz konstruktor z modyfikatorem dostepu internal
+ */
+sealed class Car
+{
+    //properties
+    public string Brand { get; }
+    public string Model { get; }
+    public double MaxSpeed { get; }
+    public int HorsePower { get; }
+    //constructor
+    internal Car(string brand, string model, double maxSpeed, int horsePower)
+    {
+        Brand = brand;
+        Model = model;
+        MaxSpeed = maxSpeed;
+        HorsePower = horsePower;
+    }
+    //methods
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Automotive;
+namespace Racing
+//Also known as: Virtual Constructor
+/*Stwórz dwa projekty
+ * 
+ * Automotive
+ * zawiera klase Car z modyfikatorami dostepu sealed oraz konstruktor z modyfikatorem dostepu internal
+ * 
+ * Racing
+ * Racing zawierający aplikacje konsolową w której będą ścigać się Car
+ * 
+ * Spraw aby możliwe było tworzenie nowych instancji klasy Car w projekcie Racing
+ */
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets Race!");
+            Car ravka = new Car("Toyota", "Rav4", 240.5, 320);
+
+            Console.WriteLine(ravka.Brand);
+        }
+    }
+}
