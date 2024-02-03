1a15b48f75ae0fc4092958d962c3509bc8eb8f7b
MarianCypher <p.rybczynski@hotmail.com>
2024-02-03 17:35:09 +0100

    Factory method applied. Racing possible

--- Car.cs
+++ Car.cs
@@ -7,17 +7,14 @@ namespace Automotive;
 sealed class Car
 {
     //properties
-    public string Brand { get; }
-    public string Model { get; }
-    public double MaxSpeed { get; }
-    public int HorsePower { get; }
-    //constructor
-    internal Car(string brand, string model, double maxSpeed, int horsePower)
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public double MaxSpeed { get; set; }
+    public int HorsePower { get; set; }
+    //constructor TO-NE-NE
+    //method
+    internal static Car CreateCar(string brand, string model, double maxSpeed, int horsePower)
     {
-        Brand = brand;
-        Model = model;
-        MaxSpeed = maxSpeed;
-        HorsePower = horsePower;
+        return new Car { Brand = brand, Model = model, MaxSpeed = maxSpeed, HorsePower = horsePower };
     }
-    //methods
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using Automotive;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection;
+
 namespace Racing
 //Also known as: Virtual Constructor
 /*Stwórz dwa projekty
@@ -17,9 +20,21 @@ namespace Racing
         static void Main(string[] args)
         {
             Console.WriteLine("Lets Race!");
-            Car ravka = new Car("Toyota", "Rav4", 240.5, 320);
+            Car firstCar = Car.CreateCar("Toyota", "Rav4", 240.5, 320);
+            Console.WriteLine("A " + firstCar.Brand + " car was created, specifically the " + firstCar.Model + " model, with a maximum speed of " + firstCar.MaxSpeed + " and a number of " + firstCar.HorsePower + " horsepower.");
+            
+            Car secondCar = Car.CreateCar("Land Rover", "Defender", 190.3, 350);
+            Console.WriteLine("A " + secondCar.Brand + " car was created, specifically the " + secondCar.Model + " model, with a maximum speed of " + secondCar.MaxSpeed + " and a number of " + secondCar.HorsePower + " horsepower.");
 
-            Console.WriteLine(ravka.Brand);
+            Console.WriteLine("Race begins");
+            if (firstCar.MaxSpeed > secondCar.MaxSpeed)
+            {
+                Console.WriteLine(firstCar.Brand + " is the WINNER!!!");
+            }
+            else
+            {
+                Console.WriteLine(secondCar.Brand + " is the WINNER!!!");
+            }
         }
     }
 }
